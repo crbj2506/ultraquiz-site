4808317bfeed4c79202a2264c25a0f15ff775020
Custódio Junior <crbj2506@gmail.com>
2022-12-14 13:24:11 -0300

    Registro desabilitado

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['verify' => true]);
+Auth::routes(['verify' => true, 'register' => false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
     ->name('home')
