28aa1282d430731b156221574332982f22d83876
Custódio Junior <crbj2506@gmail.com>
2023-01-19 21:37:53 -0300

    Correção Nomes Componentes

--- app/View/Components/IconCaretLeft.php
+++ app/View/Components/IconCaretLeft.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class Icon-clipboard extends Component
+class IconCaretLeft extends Component
 {
     /**
      * Create a new component instance.
@@ -23,6 +23,6 @@ class Icon-clipboard extends Component
      */
     public function render()
     {
-        return view('components.icon-clipboard');
+        return view('components.icon-caret-left');
     }
 }
