d6e60c8fe796413edb7a9eaeaae09406984adfb5
Custódio Junior <crbj2506@gmail.com>
2023-01-28 06:36:07 -0300

    Correção Borda

--- resources/views/partida/index.blade.php
+++ resources/views/partida/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container border ">
+<div class="container">
     <div class="row justify-content-center">
         <div class="col mt-3">
             <div class="card">
