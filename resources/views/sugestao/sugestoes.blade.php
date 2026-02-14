266b341dcf178703f92f859f5458c61eefe58c83
Custódio Junior <crbj2506@gmail.com>
2026-02-14 14:15:21 -0300

    Filtros Implementados

--- resources/views/sugestao/sugestoes.blade.php
+++ resources/views/sugestao/sugestoes.blade.php
@@ -5,6 +5,20 @@
         <div class="card">
             <div class="card-header">Lista de Sugestões de outros Jogadores para você avaliar</div>
 
+                <div class="card-header">
+                    {{-- Formulário de filtros: envia via GET para preservar parâmetros na paginação --}}
+                    <form id="formFiltroSugestao" method="GET" action="{{ route('sugestoes.listar') }}">
+                        <div class="input-group input-group-sm">
+                            <span class="input-group-text">Filtros</span>
+                            <input name="f_pergunta" type="text" class="form-control" placeholder="Parte da Pergunta" value="{{ request('f_pergunta') }}">
+                            <input name="f_resposta" type="text" class="form-control" placeholder="Parte da Resposta" value="{{ request('f_resposta') }}">
+                            <input name="f_fonte" type="text" class="form-control" placeholder="Parte da Fonte" value="{{ request('f_fonte') }}">
+                            <input name="f_criada_por" type="text" class="form-control" placeholder="Criada por (nome)" value="{{ request('f_criada_por') }}">
+                            <button type="submit" class="btn btn-sm btn-outline-primary">Filtrar</button>
+                            <a href="{{ route('sugestoes.listar') }}" class="btn btn-sm btn-outline-success">Limpar</a>
+                        </div>
+                    </form>
+                </div>
                 <table class="table">
                     <thead>
                         <tr>
